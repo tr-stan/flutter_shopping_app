6f1f3cf7470c015f8c547ccb026ff629a5faa604
Tristan Bennett <betriistan@gmail.com>
2019-09-08 23:18:07 -0500

    creates new widget for products grid, set to listen for updates in provider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // Provider.of can only be used in a widget with a(n)
+    // (in)direct parent widget that has set up a provider
+    final productsData = Provider.of<Products>(context);
+    // uses the getter in the products provider class
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(5.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
+      // allows us to define how grid should generally be structured
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
