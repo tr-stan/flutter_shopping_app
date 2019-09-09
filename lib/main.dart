3ce2f8fb03265ce2f3fc8debd5d05eea1862ba7c
Tristan Bennett <betriistan@gmail.com>
2019-09-08 23:12:03 -0500

    adds provider package/functionality to app, registered for other widgets to listen to

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview.dart';
 import './screens/product_details.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,18 +11,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'tr;stan\'s ¡Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
-        fontFamily: 'Corben',
+    return ChangeNotifierProvider(
+      // func in this provider allows any children widget to
+      // listen to any changes in the new instance of the
+      // products provider, not the whole Material app will change
+      builder: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'tr;stan\'s ¡Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+          fontFamily: 'Corben',
+        ),
+        home: ProductsOverview(),
+        routes: {
+          // '/': (ctx) => ProductsOverviewScreen(),
+          ProductDetails.routeName: (ctx) => ProductDetails(),
+        },
       ),
-      home: ProductsOverview(),
-      routes: {
-        // '/': (ctx) => ProductsOverviewScreen(),
-        ProductDetails.routeName: (ctx) => ProductDetails(),
-      },
     );
   }
 }
