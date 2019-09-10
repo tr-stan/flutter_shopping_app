599315457729d89d908b15420fa9e2d967bd65d3
Tristan Bennett <betriistan@gmail.com>
2019-09-10 00:37:29 -0500

    replaces changenotifier with multiprovider that takes list of providers for products and cart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview.dart';
 import './screens/product_details.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,8 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'tr;stan\'s ¡Shop',
         theme: ThemeData(
