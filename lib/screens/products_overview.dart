bd0a24e54695d00dc37de3ee244f2fc407d3187c
Tristan Bennett <betriistan@gmail.com>
2019-09-10 20:07:49 -0500

    adds padding to shopping cart icon/counter to make visible despite debugger banner

--- lib/screens/products_overview.dart
+++ lib/screens/products_overview.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
+import '../widgets/app_drawer.dart';
 import '../providers/cart.dart';
 import './cart_details.dart';
 
@@ -42,22 +43,29 @@ class _ProductsOverviewState extends State<ProductsOverview> {
               PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
             ],
           ),
-          Consumer<Cart>(
-            // IconButton is set as static child of Consumer and passed to
-            // its builder func so it does not rebuild when notified by provider
-            builder: (_, cart, ch) => Badge(
-              child: ch,
-              value: cart.itemCount.toString(),
-            ),
-            child: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {
-                Navigator.of(context).pushNamed(CartDetails.routeName);
-              },
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+            child: Consumer<Cart>(
+              // IconButton is set as static child of Consumer and passed to
+              // its builder func so it does not rebuild when notified by provider
+              builder: (_, cart, ch) => Badge(
+                child: ch,
+                value: cart.itemCount.toString(),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 2, 0),
+                child: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartDetails.routeName);
+                  },
+                ),
+              ),
             ),
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showFavoritesOnly),
     );
   }
