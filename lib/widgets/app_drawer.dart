846e5528ecc6b1f1ddc79449cfbbcc0feecf8c89
Tristan Bennett <betriistan@gmail.com>
2019-09-10 20:11:31 -0500

    creates drawer widget for switching b/w shopping and order history views

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../screens/order_history.dart';
+
+class AppDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
+    return ListTile(
+      leading: Icon(icon, size:26),
+      title: Text(title),
+      onTap: tapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('Drawer'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          buildListTile('Shop', Icons.shop, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          Divider(),
+          buildListTile('Orders', Icons.payment, () {
+            Navigator.of(context).pushReplacementNamed(OrderHistory.routeName);
+          }),
+        ],
+      ),
+    );
+  }
+}
